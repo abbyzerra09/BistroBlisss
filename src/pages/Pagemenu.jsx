69d8dbed0e9d3a42554195e907a3dcb204f263c6
rohit <abbyzerra@gmail.com>
2026-01-13 09:22:06 +0545

    file name changed in Menu

---
+++ src/pages/Pagemenu.jsx
@@ -0,0 +1,12 @@
+import Menu from '../components/ComponentsMenu';
+import Services from '../components/services';
+
+function PageMenu() {
+    return(
+        <>
+        <Menu/>
+        <Services/>
+        </>
+    );
+}
+export default PageMenu
