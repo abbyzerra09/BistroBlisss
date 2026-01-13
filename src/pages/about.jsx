921e69b2309561cf59533f296f7bfb34d438dfe3
rohit <abbyzerra@gmail.com>
2026-01-13 08:33:45 +0545

    customer card added in about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,11 +1,13 @@
 import AboutCard from '../components/aboutCard';
 import ChefContents from '../components/chefContents';
+import Customers from '../components/customer';
 
 function About() {
   return (
     <div className="">
       <AboutCard />
       <ChefContents />
+      <Customers />
     </div>
   );
 }
