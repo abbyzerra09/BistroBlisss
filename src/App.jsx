7e251b73ef34510bf5d293c6cc866568dd38472e
rohit <abbyzerra@gmail.com>
2026-01-13 13:28:57 +0545

    commented items removed

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ import Header from './components/header';
 import Hero from './components/heroSection';
 import ComponentsMenu from './components/ComponentsMenu';
 import AboutCard from './components/aboutCard';
-import Services from './components/services';
+import Services from './components/service';
 import ChefContents from './components/chefContents';
 import Customers from './components/customer';
 import Footer from './components/footer';
-// import Contacts from './components/contacts';
 
-import About from './pages/about'; // Import the About page
+//pages
+import About from './pages/about';
 import PageMenu from './pages/Pagemenu'
 
 function App() {
@@ -19,11 +19,9 @@ function App() {
     <Router>
       <ScrollToTop />
       
-      {/* Header and Footer stay OUTSIDE Routes so they show on every page */}
       <Header />
 
       <Routes>
-        {/* HOMEPAGE: Only show these when the URL is "/" */}
         <Route path="/" element={
           <>
             <Hero />
@@ -32,11 +30,9 @@ function App() {
             <Services />
             <ChefContents/>
             <Customers />
-            {/* <Contacts /> */}
           </>
         } />
 
-        {/* ABOUT PAGE: Only show this when the URL is "/about" */}
         <Route path="/about" element={<About />} />
         <Route path="/menu" element={<PageMenu />} />
         <Route path="/delivery" element={<ChefContents/>} />
